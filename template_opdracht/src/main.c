fdb99ca6c6c318a4867866969a02619a03c4edbc
Caspar Treijtel <c.treijtel@hva.nl>
2023-06-29 09:58:12 +0200

    Changed template code, indenting+comments

--- template_opdracht/src/main.c
+++ template_opdracht/src/main.c
@@ -1,10 +1,9 @@
-/**
- * @file main.c
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2023-06-26
- * 
+/*!
+ * \file    main.c
+ * \author  your name (you@domain.com)
+ * \brief   brief description of what program does
+ * \version 0.1
+ * \date    2023-06-26
  */
 
 #define  F_CPU 2000000UL
@@ -15,24 +14,32 @@
 #include <stdio.h>
 #include "serialF0.h"
 
-/**
- * @brief main starting point of the program.
- * @return int (with embedded this will never happen)
+/*!
+ * \brief  main starting point of the program.
+ * \return int (with embedded this will never happen)
  */
 int main()
 {
-  // Initialize the stream to be able to print with the UART
-  init_stream(F_CPU);
-  sei();
+    /////////////////////
+    //  Initialization //
+    /////////////////////
 
-  // Set pin 0 on port C as output
-  PORTC.DIRSET = PIN0_bm;
+    // Initialize the stream to be able to print with the UART
+    init_stream(F_CPU);
+    // Enable global interrupts
+    sei();
 
-  printf("Entering never ending while loop now.\n");
-  // Enter endless loop
-  while (1) {
-    // Toggle pin 0 of port C (from high to low or low to high)
-    PORTC.OUTTGL = PIN0_bm;
-    _delay_ms(100);
-  }
+    // Set pin 0 on port C as output
+    PORTC.DIRSET = PIN0_bm;
+
+
+    ///////////////////////////////
+    //  Enter endless while-loop //
+    ///////////////////////////////
+    printf("Entering never ending while loop now.\n");
+    while (1) {
+        // Toggle pin 0 of port C (from high to low or low to high)
+        PORTC.OUTTGL = PIN0_bm;
+        _delay_ms(1000);
+    }
 }
